d27cc3bc37c452f83c579ff8b78ce795df28e856
Deelip Kumar Singh <deelipsingh77@gmail.com>
2023-09-17 18:55:16 +0530

    Added Print Feature

--- script.js
+++ script.js
@@ -5,36 +5,32 @@ const fontFamilyDropdown = document.getElementById('font-family-dropdown');
 const fontColorPicker = document.getElementById('font-color-picker');
 const saveButton = document.getElementById('save-button');
 
-// Function to toggle formatting for bold, italic, underline, and strikethrough
 function toggleFormat(command) {
-    editorContainer.focus(); // Refocus on the editor container
+    editorContainer.focus();
     if (command === 'fontSize') {
         const fontSize = fontSizeDropdown.value;
         document.execCommand(command, false, fontSize);
     } else {
         document.execCommand(command, false, null);
     }
-    updateButtonStates(); // Update button states
+    updateButtonStates();
 }
 
-// Function to check and update button states for all formatting buttons
 function updateButtonStates() {
     formatButtons.forEach(button => {
         const command = button.getAttribute('data-command');
         const isActive = document.queryCommandState(command);
-        button.classList.toggle('bg-blue-500', isActive); // Highlight active buttons
-        button.classList.toggle('text-white', isActive); // Make text white for active buttons
+        button.classList.toggle('bg-blue-500', isActive);
+        button.classList.toggle('text-white', isActive);
     });
 }
 
-// Function to set font family to selected text
 function setFontFamily(fontFamily) {
     document.execCommand('fontName', false, fontFamily);
-    editorContainer.focus(); // Refocus on the editor container
-    updateButtonStates(); // Update button states
+    editorContainer.focus();
+    updateButtonStates();
 }
 
-// Function to toggle formatting for uppercase
 function toggleCase(command) {
     const selection = window.getSelection();
     if (selection) {
@@ -44,11 +40,10 @@ function toggleCase(command) {
             document.execCommand('insertText', false, transformedText);
         }
     }
-    editorContainer.focus(); // Refocus on the editor container
-    updateButtonStates(); // Update button states
+    editorContainer.focus();
+    updateButtonStates();
 }
 
-// Prevent the default behavior for certain buttons
 formatButtons.forEach(button => {
     button.addEventListener('click', (e) => {
         const command = button.getAttribute('data-command');
@@ -57,7 +52,6 @@ formatButtons.forEach(button => {
         } else if (command === 'lowercase') {
             toggleCase(command)
         } else if (command === 'fontSize') {
-            // Font size is handled in the toggleFormat function
             toggleFormat(command);
         } else if (command === 'fontName') {
             const fontFamily = button.getAttribute('data-value');
@@ -69,28 +63,22 @@ formatButtons.forEach(button => {
     });
 });
 
-// Add event listener to the save button
 saveButton.addEventListener('click', () => {
-    const content = editorContainer.innerHTML;
-    // You can implement code here to save the content to a server or perform other actions.
-    alert('Content saved successfully.');
+    window.print();
 });
 
-// Add event listener to the font size dropdown
 fontSizeDropdown.addEventListener('change', () => {
-    toggleFormat('fontSize'); // Handle font size when dropdown changes
+    toggleFormat('fontSize');
 });
 
-// Add event listener to the font family dropdown
 fontFamilyDropdown.addEventListener('change', () => {
     const fontFamily = fontFamilyDropdown.value;
     setFontFamily(fontFamily);
 });
 
-// Add event listener to the font color picker
 fontColorPicker.addEventListener('change', () => {
     const fontColor = fontColorPicker.value;
     document.execCommand('foreColor', false, fontColor);
-    editorContainer.focus(); // Refocus on the editor container
-    updateButtonStates(); // Update button states
+    editorContainer.focus();
+    updateButtonStates();
 });
